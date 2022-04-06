be0662453154899bd6a3c3c44f42a9671a4cf57a
Kaden Taylor <Kaden.Taylor@junipersys.com>
2022-04-06 08:46:01 -0600

    Ran a full test of web_scrap_all.py

--- web_scrap_all.py
+++ web_scrap_all.py
@@ -1,7 +1,8 @@
 import time
+import os
 from datetime import date
 from barcode_giant_web_scrap import barcode_giant_web_scrap
-from cdw_web_scrap import cdw_web_scrap, get_total_pages
+from cdw_web_scrap import cdw_web_scrap
 
 total_before = time.time()
 files = []
@@ -19,13 +20,15 @@ after = time.time()
 print(f"Scrapping CDW.com took: {after-before} seconds")
 
 # create one big file
-one_file = open("web_scrap_all.csv", "w")
+one_file = open(f"web_scrap_all_{str(date.today())}.csv", "w")
+one_file.write("Title, Description, Price, Web_source, Link, Date_Accessed, Part/Item #, MFG #, SKU, CDW #\n")
 for file in files:
-    print(file)
     f = open(file, 'r')
+    f.readline() # remove the header
     for line in f:
         one_file.write(line)
     f.close()
+    os.remove(file)
 one_file.close()
 
 total_after = time.time()
