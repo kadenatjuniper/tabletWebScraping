1f42b12e197cee9f2f07c30a34962a62fec2d416
Kaden Taylor <Kaden.Taylor@junipersys.com>
2022-04-12 09:44:45 -0600

    Run a test of web scrap and find price diff

--- find_price_diff.py
+++ find_price_diff.py
@@ -8,7 +8,7 @@ WEB_SOURCE = 3
 LINK = 4
 DATE_ACCESSED = 5
 
-output_file = open(f"price_diff_{str(date.today())}.csv")
+output_file = open(f"price_diff_{str(date.today())}.csv", "w")
 
 file_name_one = sys.argv[1]
 file_name_two = sys.argv[2]
@@ -20,7 +20,7 @@ f2 = open(file_name_two)
 f1.readline()
 
 # create header
-output_file.write(f"Title, Description, Date: {file_name_one[-10:-1]}, Date: {file_name_two[-10:-1]}, Source")
+output_file.write(f"Title, Description, Date: {file_name_one[-14:-4]}, Date: {file_name_two[-14:-4]}, Source\n")
 
 for f1_line in f1:
     f1_line_split = f1_line.split(',')
@@ -28,7 +28,8 @@ for f1_line in f1:
     for f2_line in f2:
         f2_line_split = f2_line.split(',')
         if f1_line_split[TITLE] == f2_line_split[TITLE] and f1_line_split[DESCRIPTION] == f2_line_split[DESCRIPTION] and f1_line_split[PRICE] != f2_line_split[PRICE]:
-            output_file.write(f"{f1_line_split[TITLE]}, {f1_line_split[DESCRIPTION]}, {f1_line_split[PRICE]}, {f2_line_split[PRICE]}, {f2_line_split[LINK]}")
+            output_file.write(f"{f1_line_split[TITLE]}, {f1_line_split[DESCRIPTION]}, {f1_line_split[PRICE]}, {f2_line_split[PRICE]}, {f2_line_split[LINK]}\n")
+    f2.seek(0)
 
 f1.close()
 f2.close()
