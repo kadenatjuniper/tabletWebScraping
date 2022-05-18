4137243698eae20567398196d8802c51fe70fd60
Kaden Taylor <Kaden.Taylor@junipersys.com>
2022-05-18 16:17:04 -0600

    working on fondriestGNSS

--- fondriestGNSS.py
+++ fondriestGNSS.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 from datetime import date
 
 
-def allterra_web_scrap():
-    print("----> STARTING: web scraping of Allterra.com")
+def fondriestGNSS():
+    print("----> STARTING: web scraping of Fondriest.com for GNSS")
     item_count = 0
-    SOURCE_WEBSITE = 'allterra'
+    SOURCE_WEBSITE = 'fondriestGNSS'
     DATE_ACCESSED = str(date.today())
 
     file_string = f"web_scrap_{SOURCE_WEBSITE}_{DATE_ACCESSED}.csv"
@@ -18,36 +18,41 @@ def allterra_web_scrap():
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
     }
 
-    URL = f"https://allterracentral.com/products.html/mapping-gis/handhelds.html"
+    URL = f"https://www.fondriest.com/products/wireless-data/gps-receivers.htm"
     r = requests.get(URL, headers=headers)
-
     soup = BeautifulSoup(r.content, 'html.parser')
+    itemsGeneral = soup.findAll('li', attrs={'class': 'item'})
 
-    items = soup.findAll('li', attrs={'class': 'item product product-item'})
+    for generalItem in itemsGeneral:
 
-    for item in items:
-        title = 'None'
-        description = 'None'
-        price = 'None'
-        link = 'None'
-        title_object = item.find('a', attrs={'class': 'product-item-link'})
-        if title_object:
-            title = title_object['title'].replace(',', '')
-            link = title_object['href']
-        model_number = ''
-        description = "allterra.com doesn't have a description with their listings"
-        price_object = item.find('span', attrs={'class': 'price'})
-        if price_object:
-            price = price_object.text.replace(',', '')
-        file.write(f"{title}, {description}, {price}, {SOURCE_WEBSITE}, {link}, {DATE_ACCESSED}, {model_number}\n")
-        item_count += 1
+        productURL = generalItem.find('a', href=True)['href']
+        r = requests.get(productURL, headers=headers)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        productTable = soup.findAll('table', attrs={'class': 'product-list-table '})
+        itemsProduct = productTable.tbody.findall('tr')
 
+        for item in itemsProduct:
+            title = 'None'
+            description = 'None'
+            price = 'None'
+            link = 'None'
+            title_object = item.find('a', attrs={'class': 'product-item-link'})
+            if title_object:
+                title = title_object['title'].replace(',', '')
+                link = title_object['href']
+            model_number = ''
+            description = "allterra.com doesn't have a description with their listings"
+            price_object = item.find('span', attrs={'class': 'price'})
+            if price_object:
+                price = price_object.text.replace(',', '')
+            file.write(f"{title}, {description}, {price}, {SOURCE_WEBSITE}, {link}, {DATE_ACCESSED}, {model_number}\n")
+            item_count += 1
 
-    print(f"----> FINISHED: Web scraping Allterra.com \nThe number of items saved to the file is {item_count}.")
+    print(f"----> FINISHED: Web scraping Fondriest.com for GNSS \nThe number of items saved to the file is {item_count}.")
 
     file.close()
     return file_string
 
 
 
-# allterra_web_scrap()
+fondriestGNSS()
