842dab898142da42a042345ee31b3f6c184526ec
Kaden Taylor <Kaden.Taylor@junipersys.com>
2022-05-19 14:18:25 -0600

    Finished creating fondriest

--- fondriestGNSS.py
+++ fondriestGNSS.py
@@ -13,7 +13,7 @@ def fondriestGNSS():
     file = open(file_string, "w")
     file.write("Title, Description, Price, Web_source, Link, Date_Accessed, Model #\n")
 
-    # This header makes it so the website doesn't realize this is a web scrapper. There is a create article with more tricks like this: https://pknerd.medium.com/5-strategies-to-write-unblock-able-web-scrapers-in-python-5e40c147bdaf
+    # This header makes it so the website doesn't realize this is a web scrapper. There is an article with more tricks like this: https://pknerd.medium.com/5-strategies-to-write-unblock-able-web-scrapers-in-python-5e40c147bdaf
     headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
     }
@@ -23,28 +23,24 @@ def fondriestGNSS():
     soup = BeautifulSoup(r.content, 'html.parser')
     itemsGeneral = soup.findAll('li', attrs={'class': 'item'})
 
+    print(f"itemsGeneral: {itemsGeneral}")
+
     for generalItem in itemsGeneral:
 
         productURL = generalItem.find('a', href=True)['href']
         r = requests.get(productURL, headers=headers)
         soup = BeautifulSoup(r.content, 'html.parser')
-        productTable = soup.findAll('table', attrs={'class': 'product-list-table '})
-        itemsProduct = productTable.tbody.findall('tr')
+        modelNumbers = soup.findAll('td', attrs={'class': 'product-list-table-sku'})
+        titles= soup.findAll('td', attrs={'class': 'product-list-table-description'})
+        prices = soup.findAll('td', attrs={'class': 'product-list-table-price'})
 
-        for item in itemsProduct:
-            title = 'None'
-            description = 'None'
-            price = 'None'
-            link = 'None'
-            title_object = item.find('a', attrs={'class': 'product-item-link'})
-            if title_object:
-                title = title_object['title'].replace(',', '')
-                link = title_object['href']
-            model_number = ''
-            description = "allterra.com doesn't have a description with their listings"
-            price_object = item.find('span', attrs={'class': 'price'})
-            if price_object:
-                price = price_object.text.replace(',', '')
+        for i in range(len(modelNumbers)):
+            title = titles[1].replace(',', '')
+            link = productURL
+            model_number = modelNumbers[1]
+            description = "fondriest.com doesn't have a description with their listings"
+            price = prices[1].replace(',', '')
+
             file.write(f"{title}, {description}, {price}, {SOURCE_WEBSITE}, {link}, {DATE_ACCESSED}, {model_number}\n")
             item_count += 1
 
