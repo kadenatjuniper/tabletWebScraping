b4492b8be4fad806b8f8110998b91b3198271f5a
Kaden Taylor <Kaden.Taylor@junipersys.com>
2022-05-23 15:35:45 -0600

    Working on scrape function in webscrape

--- webscrape.py
+++ webscrape.py
@@ -5,40 +5,31 @@ from barcodeGiant import barcodeGiant
 from cdw import cdw
 from allterra import allterra
 from barcodesInc import barcodesInc
+from fondriestGNSS import fondriestGNSS
 
 total_before = time.time()
 files = []
 
-# scrap BarcodeGiant.com
-before = time.time()
-files.append(barcodeGiant())
-after = time.time()
-print(f"Scrapping BarcodeGiant.com took: {after-before} seconds")
 
-# scrap CDW.com
-before = time.time()
-files.append(cdw())
-after = time.time()
-print(f"Scrapping CDW.com took: {after-before} seconds")
+def scrape(scrapeModule, websiteName):
+    before = time.time()
+    files.append(scrapeModule())
+    after = time.time()
+    print(f"Scrapping {websiteName} took: {after - before} seconds")
 
-# scrap Allterra.com
-before = time.time()
-files.append(allterra())
-after = time.time()
-print(f"Scrapping Allterra.com took: {after-before} seconds")
 
-# scrap BarcodesInc.com
-before = time.time()
-files.append(barcodesInc())
-after = time.time()
-print(f"Scrapping BarcodesInc.com took: {after-before} seconds")
+scrape(fondriestGNSS(), "Fondriest.com for GNSS")
+scrape(cdw(), "CDW.com")
+scrape(allterra(), "Allterra.com")
+scrape(barcodesInc(), "BarcodesInc.com")
+scrape(barcodeGiant(), "BarcodeGiant.com")
 
 # create one big file
 one_file = open(f"web_scrap_all_{str(date.today())}.csv", "w")
 one_file.write("Title, Description, Price, Web_source, Link, Date_Accessed, Model #\n")
 for file in files:
     f = open(file, 'r')
-    f.readline() # remove the header
+    f.readline()  # remove the header
     for line in f:
         one_file.write(line)
     f.close()
@@ -48,10 +39,4 @@ one_file.close()
 total_after = time.time()
 print(f"Web Scrapping All Websites took: {total_after - total_before} seconds")
 
-
-def scrape(scrapeModule, websiteName):
-    before = time.time()
-    files.append(scrapeModule())
-    after = time.time()
-    print(f"Scrapping {websiteName} took: {after - before} seconds")
 
