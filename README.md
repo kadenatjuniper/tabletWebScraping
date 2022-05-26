bdfb410ca80130c7783bca6e4980b89710a80f04
Kaden Taylor <Kaden.Taylor@junipersys.com>
2022-05-26 14:42:34 -0600

    Added a UML diagram to docs

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-Juniper System's Web Scraping Program
+# Juniper System's Web Scraping Program
 
-Purpose:
-The purpose of this program is to pull competitor pricing from various websites to create a data base of the rugged handheld device and GNSS receiver market.
+## Purpose:
+The purpose of this program is to pull competitor pricing from various websites to create a data base of price for the rugged handheld device and GNSS receiver market over time.
 
+## Git
+There is a github for this project:`https://github.com/kadenatjuniper/tabletWebScraping`
+
+# User Manual
+There is a user manual in `docs/manual.md` that explains how to use the program
+
+# Design
+There is a design document in `docs/design.md` that explains the design of this program and how to add to it. There is also a UML diagram in `docs/uml.png` that depicts that layout of the module.
+
+**Orginally Created by Kaden Taylor in Spring 2022**
--- src/webscrape.py
+++ src/webscrape.py
@@ -23,23 +23,26 @@ def scrape(scrapeModule, websiteName):
         traceback.print_exc()
 
 
+def createOneFile(files):
+    one_file = open(f"web_scrap_all_{str(date.today())}.csv", "w")
+    one_file.write("Title, Description, Price, Web_source, Link, Date_Accessed, Model #\n")
+    for file in files:
+        f = open(file, 'r')
+        f.readline()  # remove the header
+        for line in f:
+            one_file.write(line)
+        f.close()
+        os.remove(file)
+    one_file.close()
+
+
 scrape(fondriestGNSS, "Fondriest.com for GNSS")
 scrape(cdw, "CDW.com")
 scrape(allterra, "Allterra.com")
 scrape(barcodesInc, "BarcodesInc.com")
 scrape(barcodeGiant, "BarcodeGiant.com")
 
-# create one big file
-one_file = open(f"web_scrap_all_{str(date.today())}.csv", "w")
-one_file.write("Title, Description, Price, Web_source, Link, Date_Accessed, Model #\n")
-for file in files:
-    f = open(file, 'r')
-    f.readline()  # remove the header
-    for line in f:
-        one_file.write(line)
-    f.close()
-    os.remove(file)
-one_file.close()
+createOneFile(files)
 
 total_after = time.time()
 print(f"Web Scrapping All Websites took: {total_after - total_before} seconds")
